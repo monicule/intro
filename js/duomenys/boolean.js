d637ae7c8e5d7352306dcf1420f9853a26208d5d
Monika <laiskutis.monikai@gmail.com>
2024-05-02 18:40:49 +0300

    update

--- js/duomenys/boolean.js
+++ js/duomenys/boolean.js
@@ -1,5 +1,5 @@
 /*
-BOOLEAN - lgines reiksmas
+BOOLEAN - logines reiksmes
 
 - true
 - false
@@ -11,7 +11,7 @@ Boolean logikos operatoriai:
 
 */
 
-const username = "Jonas";
+const username = 'Jonas';
 const age = 99;
 const isSunShining = true;
 const hasFuel = false;
@@ -19,13 +19,56 @@ const hasFuel = false;
 console.log(isSunShining);
 console.log(hasFuel);
 
-console.log("KAIP NENAUDOTI:", true + true);
+console.log('KAIP NENAUDOTI:', (true + true) * (true + true + true));
+console.log('KAIP NENAUDOTI:', true * false);
 
-console.log("---------");
+console.log('---------------');
 console.log(true && true);
 console.log(true && false);
 console.log(false && true);
 console.log(false && false);
 
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+console.clear();
+
+console.log(true && true || true);
+console.log(true && true || false);
+
+console.log(true && false || true);
+console.log(true && false || false);
+
+console.log(false && true || true);
+console.log(false && true || false);
+
+console.log(false && false || true);
+console.log(false && false || false);
+
+console.log(true || true && true);
+console.log(true || true && false);
+
+console.log(true || false && true);
+console.log(true || false && false);
+
+console.log(false || true && true);
+console.log(false || true && false);
+
+console.log(false || false && true);
+console.log(false || false && false);
+
+console.log('---------------');
+
 console.log(!true);
 console.log(!false);
+
+// Namu darbas
+console.log('---------------')
+
+console.log(true && true && true && true);
+console.log(true && true && true && false);
+
+console.log(true && true && true || true);
+console.log(true && true && true || false);
