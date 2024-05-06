119592f9c763a7dc2fa60e8133c127dc2c9dbff6
Monika <laiskutis.monikai@gmail.com>
2024-05-06 10:28:31 +0300

    Manto uzdavinys

--- bandymai.js
+++ bandymai.js
@@ -9,11 +9,19 @@ const x = 1;
 const y = 2;
 const z = x + y;
 
-console.log(sum)
-console.log(z)
-console.log(sub)
-console.log(dauginti)
-console.log(dalinti)
+let m = 8;
+console.log(m);
+m = 9;
+console.log(m);
+m = 15;
+console.log(m);
+
+console.log(sum);
+console.log(sub);
+console.log(dauginti);
+console.log(dalinti);
+
+console.log('----------');
 
 // Stand up su Mantu
 
@@ -25,8 +33,6 @@ vardas('Mantai');
 vardas('Monika');
 vardas('Saule');
 
-
-
 function howMany(text) {
     return console.log(text.length);
 }
@@ -34,4 +40,42 @@ function howMany(text) {
 howMany('Labas');
 howMany('Gyvenimas');
 
+console.log('----------');
 
+// Uzdavinys su Mark ir John
+
+let Mmass = 78;
+let Jmass = 92;
+let Mheight = 1.69;
+let Jheight = 1.95;
+
+// pirmo var BMI formule
+let MBMI = Mmass / (Mheight * Mheight);
+let JBMI = Jmass / (Jheight * Jheight);
+console.log(MBMI);
+console.log(JBMI);
+
+//antro var BMI formule
+let MBMI2 = Mmass / Mheight ** 2;
+let JBMI2 = Jmass / Jheight ** 2;
+console.log(MBMI2);
+console.log(JBMI2);
+
+console.log('----------');
+
+let markHigherBMI = MBMI > JBMI;
+console.log(markHigherBMI);
+
+console.log('----------');
+
+Mm = 95;
+Jm = 85;
+Mh = 1.88;
+Jh = 1.76;
+let MBMI3 = Mm / (Mh * Mh);
+let JBMI3 = Jm / (Jh * Jh);
+console.log(MBMI3);
+console.log(JBMI3);
+
+let markHigherBMI3 = MBMI3 > JBMI3;
+console.log(markHigherBMI3);
