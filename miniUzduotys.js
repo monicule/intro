4148982f42e5638b6e1f7b59de41bf4553fb09d6
Monika <laiskutis.monikai@gmail.com>
2024-05-06 23:47:18 +0300

    naujas

---
+++ miniUzduotys.js
@@ -0,0 +1,42 @@
+const a = 567;
+console.log(a);
+const b = -235;
+console.log(b);
+const c = 88;
+console.log(c);
+
+const pasisveikinimas = 'Sveiki atvyke!';
+console.log(pasisveikinimas);
+const linkejimas = 'Grazios dienos!';
+console.log(linkejimas);
+const vsc = 'Jusu - Visual Studio Code';
+console.log(vsc);
+
+const lyginiai = [0, 2, 4, 6, 8];
+console.log(lyginiai);
+const temperatura = [36.6, 37.0, 38.7, 37.7, 36.8];
+console.log(temperatura);
+const nelyginiai = [1, 3, 5, 7, 9];
+console.log(nelyginiai);
+
+const darboDienos = ['pirmadienis, antradienis, treciadienis, ketvirtadienis, penktadienis'];
+console.log(darboDienos);
+const vyruVardai = ['Saulius, Paulius, Domas, Tomas, Romas'];
+console.log(vyruVardai);
+const miestai = ['Utena, Vilnius, Kaunas, Klaipeda, Ukmerge'];
+console.log(miestai);
+
+//Veiksmai su kintamaisiais
+const sum = (a + b + c);
+console.log(sum);
+
+const tekstas = 'Sveiki atvyke!' + ' ' + 'Grazios dienos!' + ' ' + 'Jusu - Visual Studio Code.';
+console.log(tekstas);
+
+const sum2 = nelyginiai[0] - nelyginiai[1] + nelyginiai[2] - nelyginiai[3] + nelyginiai [4];
+console.log('Suma', sum2);
+
+
+
+
+//Kintamuju palyginimas
