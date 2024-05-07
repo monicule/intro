efa672ea16103b40f7f7b59b21ec1da087d23b8f
Monika <laiskutis.monikai@gmail.com>
2024-05-07 15:42:37 +0300

    naujas

---
+++ js/ciklai/for.js
@@ -0,0 +1,16 @@
+/*
+FOR - pagrindinis ciklas (en. loop)
+*/
+
+console.log('START');
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+console.log('FINISH');
+
+for (let i = 0; i < 5; i++) {
+    console.log(i, 'Laba diena, su vistiena');
+
+}
