1c5078d71c62bebf408218c9a4a4603f3aee6413
Monika <laiskutis.monikai@gmail.com>
2024-05-07 15:42:44 +0300

    nd

--- miniUzduotys.js
+++ miniUzduotys.js
@@ -1,3 +1,5 @@
+// Kintamuju inicijavimas
+
 const a = 567;
 console.log(a);
 const b = -235;
@@ -5,11 +7,11 @@ console.log(b);
 const c = 88;
 console.log(c);
 
-const pasisveikinimas = 'Sveiki atvyke!';
+const pasisveikinimas = 'Sveiki!';
 console.log(pasisveikinimas);
 const linkejimas = 'Grazios dienos!';
 console.log(linkejimas);
-const vsc = 'Jusu - Visual Studio Code';
+const vsc = 'Visual Studio Code';
 console.log(vsc);
 
 const lyginiai = [0, 2, 4, 6, 8];
@@ -19,24 +21,170 @@ console.log(temperatura);
 const nelyginiai = [1, 3, 5, 7, 9];
 console.log(nelyginiai);
 
-const darboDienos = ['pirmadienis, antradienis, treciadienis, ketvirtadienis, penktadienis'];
+const darboDienos = ['pirmadienis', 'antradienis', 'treciadienis', 'ketvirtadienis', 'penktadienis'];
 console.log(darboDienos);
-const vyruVardai = ['Saulius, Paulius, Domas, Tomas, Romas'];
+const vyruVardai = ['Saulius', 'Paulius', 'Domas', 'Tomas', 'Romas'];
 console.log(vyruVardai);
-const miestai = ['Utena, Vilnius, Kaunas, Klaipeda, Ukmerge'];
+const miestai = ['Utena', 'Vilnius', 'Kaunas', 'Klaipeda', 'Ukmerge'];
 console.log(miestai);
 
-//Veiksmai su kintamaisiais
+// Veiksmai su kintamaisiais
 const sum = (a + b + c);
 console.log(sum);
 
-const tekstas = 'Sveiki atvyke!' + ' ' + 'Grazios dienos!' + ' ' + 'Jusu - Visual Studio Code.';
+const tekstas = pasisveikinimas + ' ' + linkejimas + ' ' + vsc;
 console.log(tekstas);
 
 const sum2 = nelyginiai[0] - nelyginiai[1] + nelyginiai[2] - nelyginiai[3] + nelyginiai [4];
 console.log('Suma', sum2);
+const sum3 = lyginiai[0]  - lyginiai[1] + lyginiai[2] - lyginiai [3] + lyginiai [4];
+console.log('Suma', sum3);
+const sum4 = temperatura[0] - temperatura[1] + temperatura[2] - temperatura[3] + temperatura[4];
+console.log('Suma', sum4);
+
+const sujungti = darboDienos[4] + ', ' + darboDienos[3] + ', ' + darboDienos[2] + ', ' + darboDienos[1] + ', ' + darboDienos[0];
+console.log(sujungti);
+const sujungti2 = vyruVardai[4] + ', ' + vyruVardai[3] + ', ' + vyruVardai[2] + ', ' + vyruVardai[1] + ', ' + vyruVardai[0];
+console.log(sujungti2);
+const sujungti3 = miestai[4] + ', ' + miestai[3] + ', ' + miestai[2] + ', ' + miestai[1] + ', ' + miestai[0];
+console.log(sujungti3);
 
 
-
+console.log('---------')
 
 //Kintamuju palyginimas
+
+const x = 5;
+const y = 10;
+
+if (x > y) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (x < y) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (x === y) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (x !== y) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (x >= y) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (x <= y) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+// Išvesti teksto tipo kintamųjų ilgius
+
+const name = 'Monika';
+const surname = 'Monikaite';
+
+function howMany(text) {
+    return console.log(text.length);
+}
+
+howMany(name);
+howMany(surname);
+
+// Tarpusavyje palyginti teksto tipo kintamųjų ilgius:
+
+if (name > surname) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (name < surname) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (name === surname) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (name !== surname) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (name >= surname) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (name <= surname) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+// Išvesti sąrašo tipo kintamųjų ilgius
+
+const marks = [10, 9, 8, 4, 6, 9, 9];
+const count = marks.length;
+console.log('Kiekis', count);
+
+const marks2 =[10, 8, 8, 10, 10, 6, 9];
+const count2 = marks2.length;
+console.log('Kiekis', count2);
+
+if (marks > marks2) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (marks < marks2) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (marks === marks2) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (marks !== marks2) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (marks >= marks2) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (marks <= marks2) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
