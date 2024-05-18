85cb77cc014d549443a9114abd79a37fce44cdca
Monika <laiskutis.monikai@gmail.com>
2024-05-18 22:50:34 +0300

    uzdaviniai

--- codewars/bandymai.js
+++ codewars/bandymai.js
@@ -32,15 +32,14 @@ console.log(whatday(1));
 console.log(whatday(5));
 
 // https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/train/javascript
-/*
+
 function paperwork(n, m) {
-     
-    if (n<0 || m<0);
+    if (n < 0 || m < 0) {
     return 0;
   } else {
   const sum = n * m;
   return sum;
-}
+  }
 }
 
 
@@ -48,16 +47,18 @@ console.log(paperwork(5,-5), 0, 'Failed at paperwork(5, -5)');
 console.log(paperwork(-5,-5), 0, 'Failed at paperwork(-5, -5)');
 console.log(paperwork(-5,5), 0, 'Failed at paperwork(-5, 5)');
 console.log(paperwork(5,0), 0, 'Failed at paperwork(5, 0)');
-*/
+
+
 
 // https://www.codewars.com/kata/57cc975ed542d3148f00015b/train/javascript
 
 console.log('----------');
 function check(a, x) {
-  if (a.length === 0);
-  return false;
-
-
+  if (a.includes(x)) {
+  return true;
+} else {
+    return false;
+  }
 }
 
 
@@ -66,4 +67,54 @@ console.log(check([101, 45, 75, 105, 99, 107], 107), true);
 console.log(check(['t', 'e', 's', 't'], 'e'), true);
 console.log(check(['what', 'a', 'great', 'kata'], 'kat'), false);
 
+https://www.codewars.com/kata/5861d28f124b35723e00005e/train/javascript
 
+function zeroFuel(distanceToPump, mpg, fuelLeft) {
+
+  if (distanceToPump <= mpg * fuelLeft) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(zeroFuel(50, 25, 2), true);
+console.log(zeroFuel(100, 50, 1), false);
+
+https://www.codewars.com/kata/57a429e253ba3381850000fb/train/javascript
+
+function bmi(weight, height) {
+  let bmi = weight/height ** 2;
+  if (bmi <= 18.5){
+  return 'Underweight';
+}
+
+if (bmi <= 25.0){
+  return 'Normal';
+}
+
+if (bmi <= 30.0){
+  return 'Overweight';
+}
+
+if (bmi > 30.0){
+  return 'Obese';
+}
+}
+console.log(bmi(80, 1.80), "Normal");
+
+https://www.codewars.com/kata/54d1c59aba326343c80000e7/train/javascript
+
+function solve(x, y) {
+  const solve = x / y;
+   return solve;
+}
+
+
+console.log(solve(2, 1), 2);
+console.log(solve(2, 4), 0.5);
+console.log(solve(42, 0), Infinity);
+console.log(solve(0, 8), 0);
+console.log(solve(7, 2), 3.5);
+console.log(solve(4.2, 1), 4.2);
+console.log(solve(9999, 11), 909);
