e255d0d39fb3cd1ba6d9718fa956fed18686f9ab
Monika <laiskutis.monikai@gmail.com>
2024-05-21 16:55:00 +0300

    random nd

--- js/metodai/random.js
+++ js/metodai/random.js
@@ -37,5 +37,8 @@ for (let i = 0; i < 10; i++) {
     console.log(Math.floor(Math.random() * 10) + 1);
 }
 
+console.clear();
 
 // ND: Man reikia atsitiktinio skaiciaus intervale nuo 23 iki 617 imtinai.
+
+console.log(Math.floor(Math.random() * 617 - 23 + 1)) + 23;
