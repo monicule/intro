55db7e3ceb64d486cb2a577121f14cf4b40e7d23
Monika <laiskutis.monikai@gmail.com>
2024-05-30 16:58:55 +0300

    cw

--- codewars/water.js
+++ codewars/water.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/582cb0224e56e068d800003c
 
 function litres(time) {
-    return 0;
+    return Math.floor(time * 0.5);
   }
 
 console.log(litres(2), 1, 'should return 1 litre');
@@ -11,3 +11,19 @@ console.log(litres(0.82), 0, 'should return 0 litres');
 console.log(litres(11.8), 5, 'should return 5 litres');
 console.log(litres(1787), 893, 'should return 893 litres');
 console.log(litres(0), 0, 'should return 0 litres');
+
+function litres(time) {
+  return Math.floor(time/2);
+}
+
+const litres = time => Math.floor(time * 0.5);
+
+litres = t => ~~(t / 2);
+
+function litres(time) {
+  return parseInt(time/2);
+}
+
+function litres(time) {
+  return time>>1
+}
