b87a92dd0579bb5a69008f966895ffa9a7d17808
Monika <laiskutis.monikai@gmail.com>
2024-06-10 17:18:24 +0300

    cw

--- codewars/pitagoras.js
+++ codewars/pitagoras.js
@@ -15,16 +15,24 @@ console.log(isPythagoreanTriple([3, 4, 5]), true);
 console.log(isPythagoreanTriple([3, 5, 9]), false);
 
 
-function isPythagoreanTriple2(integers) {
-    let a = [];
-    if (a[0]*a[0] + a[1]*a[1] === a[2]*a[2] || 
-        a[0]*a[0] + a[2]*a[2] === a[1]*a[1] || 
-        a[1]*a[1] + a[2]*a[2] === a[0]*a[0]) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+function isPythagoreanTriple(integers) {
+  let [a, b, c] = integers.sort((a, b) => a - b);
+  return a * a + b * b === c * c;
+}
 
 console.log(isPythagoreanTriple2([3, 4, 5]), true);
 console.log(isPythagoreanTriple2([3, 5, 9]), false);
+
+function isPythagoreanTriple(n) {
+  const [a, b, c] = n.sort((a, b) => a-b);
+  return a**2 + b**2 === c**2;
+}
+
+function isPythagoreanTriple(i) {
+  if (i[0]**2 + i[1]**2 === i[2]**2 ||
+      i[0]**2 + i[2]**2 === i[1]**2 ||
+      i[1]**2 + i[2]**2 === i[0]**2) {
+    return true;
+  }
+  return false;
+}
